656e99928798329f05342ab824f155c30d8cc8a1
npelmore <nate.b.pelmore@gmail.com>
2021-03-22 19:18:43 -0700

    Implemented taking multiple Q's

--- ChatBotA3/src/main.java
+++ ChatBotA3/src/main.java
@@ -13,29 +13,6 @@ public class main {
 
 	public static void main(String[] args) {
 		new Window();
-		//test("abcd");
-	}
-
-	public static void test(String s) {
-
-		    Annotation document = new Annotation("Barack Obama was born in Hawaii.  He is the president. Obama was elected in 2008.");
-		    Properties props = new Properties();
-		    props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,coref");
-		    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-		    pipeline.annotate(document);
-		    System.out.println("---");
-		    System.out.println("coref chains");
-		    for (CorefChain cc : document.get(CorefCoreAnnotations.CorefChainAnnotation.class).values()) {
-		      System.out.println("\t" + cc);
-		    }
-		    for (CoreMap sentence : document.get(CoreAnnotations.SentencesAnnotation.class)) {
-		      System.out.println("---");
-		      System.out.println("mentions");
-		      for (Mention m : sentence.get(CorefCoreAnnotations.CorefMentionsAnnotation.class)) {
-		        System.out.println("\t" + m);
-		       }
-		    }
-		  
 	}
 }
 
